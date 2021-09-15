86bcefd48364df85a888a170a1a669e91d433be0
ded-git <sakarailko@gmail.com>
2021-09-15 17:55:32 +0300

    start

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from './Header';
+import Sidebar from './Sidebar';
+import Footer from './Footer';
+
+const App = () => {
+  return (
+  <div className='app-wrapper'>
+    <Header />
+    <Sidebar />
+    <Footer />
+
+  </div>
+  );
+}
+
+export default App;
